982a5fccbb3f41fdf3dde867b09f6246835825e4
writer0713 <writer0713@naver.com>
2024-05-31 20:10:42 +0900

    refactor: Jpa..Repository 모두 infra 패키지로 이동 - 특정기술을 구현한 Jpa...Repository 는 infra 로 이동

--- src/main/java/kitchenpos/menu_group/infra/JpaMenuGroupRepository.java
+++ src/main/java/kitchenpos/menu_group/infra/JpaMenuGroupRepository.java
@@ -1,5 +1,7 @@
-package kitchenpos.menu_group.domain;
+package kitchenpos.menu_group.infra;
 
+import kitchenpos.menu_group.domain.MenuGroup;
+import kitchenpos.menu_group.domain.MenuGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
--- src/main/java/kitchenpos/order_table/infra/JpaOrderTableRepository.java
+++ src/main/java/kitchenpos/order_table/infra/JpaOrderTableRepository.java
@@ -1,5 +1,7 @@
-package kitchenpos.order_table.domain;
+package kitchenpos.order_table.infra;
 
+import kitchenpos.order_table.domain.OrderTable;
+import kitchenpos.order_table.domain.OrderTableRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
